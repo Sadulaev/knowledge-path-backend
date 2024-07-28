b307bea664bd0baae5a96ce4f6f5541a94eeb767
Sadulaev <ismailsadulaev1999@mail.ru>
2024-07-28 23:34:10 +0300

    Updated data-source, added user entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,17 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-
+import { User } from './entities/user.entity';
+import 'dotenv/config';
 
 const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.HOST,
     port: +process.env.DB_PORT!,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB,
+    entities: [User],
     synchronize: true,
-    logging: true
 })
 
 export default AppDataSource;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express, { Express, Request, Response } from "express";
+import AppDataSource from "./data-source";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log(AppDataSource.options)
+
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = +process.env.SERVER_PORT! || 3000;
+AppDataSource.initialize();
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + Typescript server");
 });
 
 app.listen(port, () => {
-    console.log('[server]: Server is running on port: ' + { port })
+    console.log('[server]: Server is running on port: ' + port)
 })
